a976b5bca2470b48951c7a28c42d0253697a3df2
Borgave, Akshay Deepak (SVIC 27) <akshay.borgave@credit-suisse.com>
2020-05-02 19:31:37 +0530

    build fail fix

--- app/build.gradle.kts
+++
@@ -1,3 +0,0 @@
-apply plugin: 'com.android.application'
-apply from: "$project.rootDir/configurations/android-base.gradle"
-
--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
 
         // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle.kts files
+        // in the individual module build.gradle files
     }
 }
 
